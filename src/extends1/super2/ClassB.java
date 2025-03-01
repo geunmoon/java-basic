813ce1a70d876339f8b895b0f11e4c4a6afb5c4e
geunmoon <geunmooon@gmail.com>
2025-03-01 18:52:52 +0900

    section10(super)수정

--- src/extends1/super2/ClassB.java
+++ src/extends1/super2/ClassB.java
@@ -2,9 +2,10 @@ package extends1.super2;
 
 public class ClassB extends ClassA {
     public ClassB(int a) {
-        super();
+        //super();
         //상속관계에서 부모의 생성자를 호출할 때
         //부모 클래스 기본 생성자 사용 시 생략가능
+        this(a, 0);//이 생성자 맑 나의 다른 생성자 호출해 달라는 뜻 -> 결국 super이 한번은 나와야함
         System.out.println("ClassB 생성자 a=" + a);
     }
     public ClassB(int a, int b) {
--- src/extends1/super2/Super2Main.java
+++ src/extends1/super2/Super2Main.java
@@ -2,7 +2,7 @@ package extends1.super2;
 
 public class Super2Main {
     public static void main(String[] args) {
-        ClassC c = new ClassC();
+        //ClassC c = new ClassC();
         ClassB b = new ClassB(100);
     }
 }
